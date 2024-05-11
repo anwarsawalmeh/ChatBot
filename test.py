fb89d835e14bd7940bf48d8afe3eff2d7db208d3
Anwat <asawalmeh2004@gmail.com>
2024-05-11 17:50:26 +0100

    Created the first two stages of the NLP Preprocessing Pipeline - Tokenizing & Stemming

---
+++ test.py
@@ -0,0 +1,24 @@
+import nltk
+from nltk.stem.porter import PorterStemmer
+stemmer = PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+
+def stem(word):
+    return stemmer.stem(word.lower()    )
+
+def bag_of_words(tokenized_words, all_words):
+    pass
+
+
+
+stri = "How long will shipping take?"
+print(stri)
+print(tokenize(stri))
+token = tokenize(stri)
+l = []
+for x in token:
+    l.append(stem(x))
+    
+print(l)
